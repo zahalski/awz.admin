a1f5c075befe519eb9641304637e21069c9d1534
Andrew Zahalski <info@zahalski.dev>
2023-07-19 11:34:48 +0300

    - sql error

--- update/1.1.2/updater.php
+++ update/1.1.2/updater.php
@@ -2,7 +2,7 @@
 $moduleId = "awz.admin";
 if(IsModuleInstalled($moduleId)) {
 $connection = \Bitrix\Main\Application::getConnection();
-$sql = 'CREATE TABLE IF NOT EXISTS `b_awz_admin_gens` (`ID` int(18) NOT NULL AUTO_INCREMENT, `NAME` varchar(256) NOT NULL, `ADM_LINK` varchar(256) NOT NULL, `ADD_DATE` datetime NOT NULL, `PRM` longtext NOT NULL, PRIMARY KEY (`ID`)';
+$sql = 'CREATE TABLE IF NOT EXISTS `b_awz_admin_gens` (`ID` int(18) NOT NULL AUTO_INCREMENT, `NAME` varchar(256) NOT NULL, `ADM_LINK` varchar(256) NOT NULL, `ADD_DATE` datetime NOT NULL, `PRM` longtext NOT NULL, PRIMARY KEY (`ID`))';
 $connection->queryExecute($sql);
 
 $updater->CopyFiles(
