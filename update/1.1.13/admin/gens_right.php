6cc751ba65ab36754407d4331cc9d992d1b51720
Andrew Zahalski <info@zahalski.dev>
2024-12-02 08:14:19 +0300

    gulp checkpoint

---
+++ update/1.1.13/admin/gens_right.php
@@ -0,0 +1,31 @@
+<?
+require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_admin_before.php");
+use Bitrix\Main\Localization\Loc;
+use Bitrix\Main\Loader;
+use Awz\Admin\Access\AccessController;
+use Awz\Admin\Access\Custom\ActionDictionary;
+
+global $APPLICATION;
+$dirs = explode('/',dirname(__DIR__ . '../'));
+$module_id = array_pop($dirs);
+unset($dirs);
+Loc::loadMessages(__FILE__);
+
+if(!Loader::includeModule('awz.admin')) return;
+if(!Loader::includeModule($module_id)) return;
+
+if(!AccessController::can(0, ActionDictionary::ACTION_GENS_RIGHT))
+    $APPLICATION->AuthForm(Loc::getMessage("ACCESS_DENIED"));
+
+/* "Awz\Admin\AdminPages\PageItemEdit" replace generator */
+use Awz\Admin\AdminPages\GensRight as PageItemEdit;
+
+$APPLICATION->SetTitle(PageItemEdit::getTitle());
+$arParams = PageItemEdit::getParams();
+
+include($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/awz.admin/include/handler_el.php");
+/* @var bool $customPrint */
+if(!$customPrint) {
+    $adminCustom = new PageItemEdit($arParams);
+    $adminCustom->defaultInterface();
+}
---
+++ update/1.1.13/install/version.php
@@ -0,0 +1,6 @@
+<?
+$arModuleVersion = array(
+    "VERSION" => "1.1.13",
+    "VERSION_DATE" => "2024-12-02 08:12:00"
+);
+?>
