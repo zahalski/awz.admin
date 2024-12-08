b531cb31494c0ed902258660a2d863f666b8f0c9
Andrew Zahalski <info@zahalski.dev>
2024-12-08 11:04:13 +0300

    gulp checkpoint

---
+++ update/1.2.1/install/version.php
@@ -0,0 +1,6 @@
+<?
+$arModuleVersion = array(
+    "VERSION" => "1.2.1",
+    "VERSION_DATE" => "2024-12-08 11:02:00"
+);
+?>
---
+++ update/1.2.1/lib/access/custom/rules/gensright.php
@@ -0,0 +1,24 @@
+<?php
+namespace Awz\Admin\Access\Custom\Rules;
+
+use Bitrix\Main\Access\AccessibleItem;
+use Awz\Admin\Access\Custom\PermissionDictionary;
+use Awz\Admin\Access\Custom\Helper;
+
+class Gensright extends \Bitrix\Main\Access\Rule\AbstractRule
+{
+
+    public function execute(AccessibleItem $item = null, $params = null): bool
+    {
+        if ($this->user->isAdmin())
+        {
+            return true;
+        }
+        if ($this->user->getPermission(PermissionDictionary::GENS_RIGHT))
+        {
+            return true;
+        }
+        return false;
+    }
+
+}
